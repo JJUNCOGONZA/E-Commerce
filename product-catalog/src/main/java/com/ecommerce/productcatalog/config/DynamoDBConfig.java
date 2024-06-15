c6de9d3d6b8aff79b1809b883917f6f4bb69b5e8
JJUNCOGONZA <jeissonjuncogonzalez@gmail.com>
2024-06-15 15:52:46 -0500

    add temporary configuration for docker setup

--- product-catalog/src/main/java/com/ecommerce/productcatalog/config/DynamoDBConfig.java
+++ product-catalog/src/main/java/com/ecommerce/productcatalog/config/DynamoDBConfig.java
@@ -1,5 +1,5 @@
 package com.ecommerce.productcatalog.config;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -12,13 +12,25 @@ import java.net.URI;
 @Configuration
 public class DynamoDBConfig {
 
+    @Value("${dynamodb.endpoint}")
+    private String dynamoDbEndpoint;
+
+    @Value("${dynamodb.region}")
+    private String dynamoDbRegion;
+
+    @Value("${dynamodb.accessKey}")
+    private String accessKey;
+
+    @Value("${dynamodb.secretKey}")
+    private String secretKey;
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .endpointOverride(URI.create("http://localhost:4566"))
-                .region(Region.US_EAST_1)
+                .endpointOverride(URI.create(dynamoDbEndpoint))
+                .region(Region.of(dynamoDbRegion))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("test", "test")))
+                        AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
